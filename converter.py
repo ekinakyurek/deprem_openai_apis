e670e3ce167c64d173b12b46c372d905088d1b97
Ekin Akyürek <akyurekekin@gmail.com>
2023-02-07 10:34:06 -0500

    update to new format

--- converter.py
+++ converter.py
@@ -3,7 +3,9 @@ import os
 import openai
 from absl import app
 from absl import flags
+from absl import logging
 import time
+from tqdm import tqdm
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -44,35 +46,60 @@ def query_with_retry(inputs, max_retry=2):
             current_outputs = response["choices"]
             outputs = []
             for i in range(len(current_outputs)):
-                for line in current_outputs[i]["text"].split("\n"):
-                    outputs.append(line)
+                outputs.append(
+                    [
+                        line
+                        for line in current_outputs[i]["text"].split("\n")
+                        if len(line) > 10
+                    ]
+                )
             request_completed = True
         except Exception as e:
-            print("Error:", e)
+            logging.warning(f"Error: {e}")
             # wait for token limit in the API
             time.sleep(30)
+            current_retry += 1
     return outputs
 
 
 def main(_):
     with open(FLAGS.prompt_file) as handle:
         template = handle.read()
+    # extract tweets from third columns
+    # raw_data = pd.read_csv(FLAGS.input_file)
 
     with open(FLAGS.input_file) as handle:
-        raw_data = [json.loads(line)["input"] for line in handle]
+        raw_data = json.load(handle)["hits"]["hits"]
+        # raw_data = [json.loads(line)["input"] for line in handle]
 
-    current_inputs = []
-    processed_data = []
-    for i, datum in enumerate(raw_data):
-        current_inputs.append(template.format(ocr_input=datum))
-        if (i + 1) % 20 == 0 or i == len(raw_data) - 1:
-            current_outputs = query_with_retry(current_inputs)
-            processed_data += current_outputs
-            current_inputs = []
+    logging.info(f"Length of the data is {len(raw_data)}")
+    text_inputs = []
+    raw_inputs = []
 
-    with open(FLAGS.output_file, "w") as handle:
-        for out in processed_data:
-            handle.write(out + "\n")
+    # if os.file.exists(FLAGS.output_file):
+    #     os.error("Output file exists!")
+
+    for index, row in tqdm(enumerate(raw_data)):
+        # third columns is the tweet?
+        text_inputs.append(template.format(ocr_input=row["_source"]["text"]))
+        raw_inputs.append(row)
+
+        if (index + 1) % 4 == 0 or index == len(raw_data) - 1:
+            outputs = query_with_retry(text_inputs)
+            with open(FLAGS.output_file, "a+") as handle:
+                for inp, addresses in zip(raw_inputs, outputs):
+                    for address in addresses:
+                        current_input = inp.copy()
+                        try:
+                            current_input["address_json"] = json.loads(address)
+                            current_input["address_str"] = ""
+                        except Exception as e:
+                            logging.warning(f"Parsing error in {address},\n {e}")
+                            current_input["address_json"] = {}
+                            current_input["address_str"] = address
+
+                        json_output = json.dumps(current_input)
+                        handle.write(json_output + "\n")
 
 
 if __name__ == "__main__":
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python converter.py --prompt_file prompts/main.txt --input_file data/test.jsonl --output_file output.jsonl
+python converter.py --prompt_file prompts/main.txt --input_file data/test.json --output_file output.jsonl
