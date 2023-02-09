806904fb237c4fadecab9b5338362de3e7bcc30f
Ekin Akyürek <akyurekekin@gmail.com>
2023-02-09 01:41:45 -0500

    update

--- converter.py
+++ converter.py
@@ -1,8 +1,11 @@
 import json
 import os
 import time
+import urllib
 import backoff
 import openai
+import requests
+import re
 from absl import app, flags, logging
 from tqdm import tqdm
 
@@ -17,13 +20,101 @@ flags.DEFINE_string(
 
 flags.DEFINE_string("input_file", default=None, help="Input file to read data")
 
-flags.DEFINE_string("key_file", default=None, help="Openai key file")
+# flags.DEFINE_string("key_file", default=None, help="Openai key file")
 
 flags.DEFINE_string("output_file", default=None, help="Output file to write to")
 
 flags.DEFINE_integer("max_tokens", default=384, help="LM max generation length")
 
+flags.DEFINE_integer("worker_id", default=0, help="Worker id for the job")
+
+flags.DEFINE_integer("num_workers", default=1, help="number of workers")
+
+flags.DEFINE_integer("batch_size", default=20, help="batch size for OpenAI queries")
+
+flags.DEFINE_boolean(
+    "geo_location", default=False, help="whether to add geo location to the output"
+)
+
+flags.DEFINE_string("info", default="address", help="address | intent")
+
 flags.DEFINE_string("engine", "code-cushman-001", help="GPT engines")
+
+GEO_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?"
+
+NON_ADDRESS_WORDS = [
+    "arkadaş",
+    "bebek",
+    "enkaz",
+    "deprem",
+    "ekipman",
+    "araç",
+    "kayıp",
+    "acil",
+    "yardım",
+    "kurtarma",
+    "kayıp",
+    "aile",
+    "baba",
+]
+
+
+def postprocess_for_address(address):
+    # small rule based filtering
+    address = json.loads(address)
+    if type(address) == dict:
+        for key in (
+            "mahallesi | bulvarı",
+            "sokak | caddesi | yolu",
+            "sitesi | apartmanı",
+            "no | blok",
+            "kat",
+            "phone",
+        ):
+            if (
+                key in address
+                and len(address[key]) > 50
+                or any(word in address[key] for word in NON_ADDRESS_WORDS)
+            ):
+                address[key] = ""
+
+        for key in ("no | blok", "kat"):
+            if key in address and len(address[key]) > 20:
+                address[key] = ""
+
+    return address
+
+
+def postprocess_for_intent(intent):
+    m = re.search(r"(?<=\[).+?(?=\])", intent)
+    if m:
+        return {"intent": m.group()}
+    else:
+        return {"intent": "unknown"}
+
+
+def postprocess(info, line):
+    if info == "address":
+        return postprocess_for_address(line)
+    elif info == "intent":
+        return postprocess_for_intent(line)
+    else:
+        raise ValueError("Unknown info type")
+
+
+def get_address_str(address):
+    address_str = ""
+    for key in (
+        "mahallesi | bulvarı",
+        "sokak | caddesi | yolu",
+        "sitesi | apartmanı",
+        "no | blok",
+        "city",
+        "province",
+    ):
+        address_str += address.get(key, "") + " "
+
+    return address_str.strip()
 
 
 def query_with_retry(inputs, max_retry=5):
@@ -54,7 +145,7 @@ def query_with_retry(inputs, max_retry=5):
                     ]
                 )
             request_completed = True
-            logging.info("request completed")            
+            logging.info("request completed")
         except openai.error.RateLimitError as error:
             logging.warning(f"Rate Limit Error: {error}")
             # wait for token limit in the API
@@ -68,26 +159,82 @@ def query_with_retry(inputs, max_retry=5):
     return outputs
 
 
-def main(_):
-    with open(FLAGS.key_file) as handle:
-        api_key, *extras = handle.readlines()
-        
-    openai.api_key = api_key.strip()
+def setup_openai():
+    try:
+        openai_keys = os.environ["OPENAI_API_KEY_POOL"].split(",")
+    except KeyError:
+        logging.error("OPENAI_API_KEY_POOL environment variable is not specified")
 
-    if extras and len(extras[0]) > 1:
-        logging.info(extras[0])
-        openai.organization = extras[0].strip()
+    assert len(openai_keys) > 0, "No keys specified in the environment variable"
+
+    worker_openai_key = openai_keys[FLAGS.worker_id % len(openai_keys)].strip()
+    openai.api_key = worker_openai_key
+
+
+def setup_geocoding():
+    try:
+        geo_keys = os.environ["GEO_KEY_POOL"].split(",")
+    except KeyError:
+        logging.error("GEO_KEY_POOL environment variable is not specified")
+
+    assert len(geo_keys) > 0, "No keys specified in the environment variable"
+
+    worker_geo_key = geo_keys[FLAGS.worker_id % len(geo_keys)].strip()
+
+    return worker_geo_key
+
+
+def get_geo_result(key, address):
+    address_str = get_address_str(address)
+    parameters = {"address": address_str, "key": key}
+    response = requests.get(f"{GEO_BASE_URL}{urllib.parse.urlencode(parameters)}")
+
+    if response.status_code == 200:
+        results = json.loads(response.content)["results"]
+        if results:
+            for result in results:
+                if "geometry" in result and "location" in result["geometry"]:
+                    loc = result["geometry"]["location"]
+                    link = "https://maps.google.com/?q={lat},{lng}".format(
+                        lat=loc["lat"], lng=loc["lng"]
+                    )
+                    result["gmaps_link"] = link
+        return results
+    else:
+        logging.warning(response.content)
+
+
+def main(_):
+    # with open(FLAGS.key_file) as handle:
+    #     api_key, *extras = handle.readlines()
+
+    # openai.api_key = api_key.strip()
+
+    # if extras and len(extras[0]) > 1:
+    #     logging.info(extras[0])
+    #     openai.organization = extras[0].strip()
+
+    setup_openai()
+    if FLAGS.geo_location:
+        geo_key = setup_geocoding()
 
     with open(FLAGS.prompt_file) as handle:
         template = handle.read()
+
     # extract tweets from third columns
     # raw_data = pd.read_csv(FLAGS.input_file)
 
     with open(FLAGS.input_file) as handle:
-        raw_data = json.load(handle)["hits"]["hits"]
+        # raw_data = json.load(handle)["hits"]["hits"]
+        #raw_data = json.load(handle)
+        raw_data = [json.loads(line.strip()) for line in handle]
+        split_size = len(raw_data) // FLAGS.num_workers
+        raw_data = raw_data[
+            FLAGS.worker_id * split_size : (FLAGS.worker_id + 1) * split_size
+        ]
         # raw_data = [json.loads(line)["input"] for line in handle]
 
-    logging.info(f"Length of the data is {len(raw_data)}")
+    logging.info(f"Length of the data for this worker is {len(raw_data)}")
     text_inputs = []
     raw_inputs = []
 
@@ -96,25 +243,38 @@ def main(_):
 
     for index, row in tqdm(enumerate(raw_data)):
         # third columns is the tweet?
-        text_inputs.append(template.format(ocr_input=row["_source"]["text"]))
+        text_inputs.append(template.format(ocr_input=row["Tweet"]))
         raw_inputs.append(row)
 
-        if (index + 1) % 20 == 0 or index == len(raw_data) - 1:
+        if (index + 1) % FLAGS.batch_size == 0 or index == len(raw_data) - 1:
             outputs = query_with_retry(text_inputs)
+
             with open(FLAGS.output_file, "a+") as handle:
-                for inp, addresses in zip(raw_inputs, outputs):
-                    for address in addresses:
+                for inp, output_lines in zip(raw_inputs, outputs):
+                    for output_line in output_lines:
                         current_input = inp.copy()
                         try:
-                            current_input["address_json"] = json.loads(address)
-                            current_input["address_str"] = ""
+                            current_input[
+                                FLAGS.info + "_json"
+                            ] = postprocess(FLAGS.info, output_line)
+                            current_input[FLAGS.info + "_str"] = ""
                         except Exception as e:
-                            logging.warning(f"Parsing error in {address},\n {e}")
-                            current_input["address_json"] = {}
-                            current_input["address_str"] = address
+                            logging.warning(f"Parsing error in {output_line},\n {e}")
+                            current_input[FLAGS.info + "_json"] = {}
+                            current_input[FLAGS.info + "_str"] = output_line
+
+                        if (
+                            FLAGS.info == "address"
+                            and FLAGS.geo_location
+                            and type(current_input[FLAGS.info + "_json"]) == dict
+                        ):
+                            current_input["geo"] = get_geo_result(
+                                geo_key, current_input[FLAGS.info + "_json"]
+                            )
 
                         json_output = json.dumps(current_input)
                         handle.write(json_output + "\n")
+
             text_inputs = []
             raw_inputs = []
 
---
+++ run_intent.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+EXP_NAME="intent"
+BASE_PATH="/home/akyurek/deprem/"
+INPUTFILE="data/test.jsonl"
+NUMKEY=5
+
+# # First split data into separate files
+# mkdir -p data/${EXP_NAME}/
+# python data_split.py --input_file $INPUTFILE --key_num $NUMKEY --output_dir data/${EXP_NAME}/
+
+# Then run the converter
+
+
+for i in $(seq 0 $((NUMKEY-1)));
+do
+    OUTPUT_PATH=$BASE_PATH/exps/${EXP_NAME}/${i}/
+    mkdir -p $OUTPUT_PATH
+    python converter.py \
+    --prompt_file prompts/intent.txt \
+    --input_file $INPUTFILE \
+    --output_file $OUTPUT_PATH/output.jsonl \
+    --worker_id $i \
+    --info="intent" \
+    --num_workers $NUMKEY > $OUTPUT_PATH/out.log 2>&1 &
+done
+
+
