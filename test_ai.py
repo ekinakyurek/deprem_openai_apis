067e709d9a773174c5cf3a387f9fc828cd1ff2da
Zafer Çavdar <zafercavdar@yahoo.com>
2023-02-12 22:14:38 +0300

    Use global payload

--- test_ai.py
+++ test_ai.py
@@ -11,11 +11,10 @@ PAYLOAD = {
         " #depremhatay #deprem #Hatay #hatayacil #HatayaYardım #hataydepremi",
         "LÜTFEN YAYIN!!!! 8 katlı bina HATAYDA Odabaşı mah. Uğur Mumcu caddesi no 4"
         " Mahmut Karakaş kat 4",
-    ]
+    ],
 }
 
 
 def test_intent():
-    payload = PAYLOAD
-    response = client.post("/intent-extractor/", json=payload)
+    response = client.post("/intent-extractor/", json=PAYLOAD)
     assert response.status_code == 200
